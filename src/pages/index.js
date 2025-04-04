c0e844466955dad43b5debc985d2d9a17dd371be
wood <woodpeng@gmail.com>
2025-04-04 23:31:03 +0800

    add google account detect and login

--- src/pages/index.js
+++ src/pages/index.js
@@ -899,6 +899,33 @@ export default function Home() {
     gtag('config', 'G-RYTCZEQK0W');
   }, []);
 
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://accounts.google.com/gsi/client";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      // Initialize Google Identity Services
+      window.google.accounts.id.initialize({
+        client_id: '988058218123-enpfsi0n6fo9jqa2aqfr6s37t16loth8.apps.googleusercontent.com',
+        callback: handleCredentialResponse
+      });
+      window.google.accounts.id.prompt(); // Automatically prompt for login
+    };
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const handleCredentialResponse = (response) => {
+    // Handle the response from Google
+    console.log("Encoded JWT ID token: " + response.credential);
+    window.open('https://app.funblocks.net/#/login?g_login_token='+response.credential, '_blank')
+  };
+
   return (
     <Layout
       title={translate({
